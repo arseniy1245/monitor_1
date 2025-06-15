6c9e7a96db21b059d1b45b0191044abd1ecc4cbd
Arseniy <arseniy.gll@mail.ru>
2025-06-15 10:51:16 +0300

    1

---
+++ js.js
@@ -0,0 +1,17 @@
+
+const fetch = require('node-fetch');
+// const cheerio = require('cheerio');
+// const fs = require('fs');
+
+
+async function a() {
+const response = await fetch('https://tlp-ab.ru/bureau/');
+let html = await response.text();
+console.log(html)
+}
+a()
+
+
+
+// const url = 'https://tlp-ab.ru/bureau/'; // Замените на нужный URL
+// fetchAndParse(url);
